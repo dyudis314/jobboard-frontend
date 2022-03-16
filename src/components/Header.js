beb80b6aedab75ccabb6f28a7ee7399b4aa996c8
Dylan Cole <66389199+dyudis314@users.noreply.github.com>
2022-03-16 08:53:56 -0700

    format form and header text on post

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,45 +6,45 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 
-const Header = ({jobs, setJobs, jobCategory, setJobCategory, setFilteredJobs}) => {
+const Header = ({ jobs, setJobs, jobCategory, setJobCategory, setFilteredJobs }) => {
 
     useEffect(() => {
         filterJobs();
-        },[jobs, jobCategory])
-       
+    }, [jobs, jobCategory])
+
     const filterJobs = () => {
-    if (jobCategory === 'Full-Stack') {
-        setFilteredJobs(jobs.filter(job => job.category === 'Full-Stack'))
-    } else if (jobCategory === 'Frontend') {
-        setFilteredJobs(jobs.filter(job => job.category === 'Frontend'))
-    } else if (jobCategory === 'Backend') {
-        setFilteredJobs(jobs.filter(job => job.category === 'Backend'))
-    } else {
-        setFilteredJobs(jobs)
+        if (jobCategory === 'Full-Stack') {
+            setFilteredJobs(jobs.filter(job => job.category === 'Full-Stack'))
+        } else if (jobCategory === 'Frontend') {
+            setFilteredJobs(jobs.filter(job => job.category === 'Frontend'))
+        } else if (jobCategory === 'Backend') {
+            setFilteredJobs(jobs.filter(job => job.category === 'Backend'))
+        } else {
+            setFilteredJobs(jobs)
         }
     }
 
     return (
-    <Stack className="header-box" spacing={2}>
-     <div>
-        <Typography sx={{ fontSize: 55 }} className="maintext">
-            Want to find your next dev role?
-        </Typography>
-     </div>
-     <div>
-        <Typography sx={{ fontSize: 20, mb: 3, color: '#534737' }} className="header-subtext">
-            Let's get toasty!
-        </Typography>
-     </div>
-     <div>
-        <Form
-        jobs={jobs}
-        setJobs={setJobs}
-        jobCategory={jobCategory}
-        setJobCategory={setJobCategory}
-        />
-     </div>
-    </Stack>
+        <Stack className="header-box" spacing={2}>
+            <div>
+                <Typography sx={{ fontSize: 55 }} className="maintext">
+                    Want to find your next dev role?
+                </Typography>
+            </div>
+            <div>
+                <Typography sx={{ fontSize: 20, mb: 3, color: '#534737' }} className="header-subtext">
+                    Let's get toasty!
+                </Typography>
+            </div>
+            <div>
+                <Form
+                    jobs={jobs}
+                    setJobs={setJobs}
+                    jobCategory={jobCategory}
+                    setJobCategory={setJobCategory}
+                />
+            </div>
+        </Stack>
     );
 }
 export default Header
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,56 +1,64 @@
-import { BrowserRouter as Link} from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import '../components/Header.css';
 import '../index.css';
-import { Typography, Card, CardContent, Grid, TextField } from "@material-ui/core";
+import { Card, CardContent, Grid, TextField } from "@material-ui/core";
+import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { yellow } from '@mui/material/colors';
+
+const colorPrimary = yellow[600];
 
 
 export default function Post() {
-    return (
-      <div>
-         <Navbar>
-          <Link to="/about"/>
-          <Link to="/contact" />
-          <Link to="/resources"/>
-        </Navbar>
-        <Stack className="header-box" spacing={2}>
-          <div>
-            <Typography sx={{ fontSize: 50 }} className="maintext">
-                Looking to post a job?
-            </Typography>
-          </div>
-          <div>
-            <Typography sx={{ fontSize: 20, mb: 3, color: '#534737' }} className="header-subtext">
-                Fill out the below form!
-            </Typography>
-          </div>
-          </Stack>
-          <Card>
-            <CardContent>
-              <Grid container spacing={1}>
-                <Grid xs={12} sm={6} item>
-                  <TextField label="Company Name" placeholder="Enter company name" variant="outlined" fullWidth required></TextField>
-                </Grid>
-                <Grid xs={12} sm={6} item>
-                  <TextField label="Job Title" placeholder="Enter job title" variant="outlined" fullWidth required></TextField>
-                </Grid>
-                <Grid xs={12} item>
-                  <TextField type="email" label="Email" placeholder="Enter email" variant="outlined" fullWidth required></TextField>
-                </Grid>
-                <Grid xs={12} item>
-                  <TextField type="number" label="Phone" placeholder="Enter phone number" variant="outlined" fullWidth required></TextField>
-                </Grid>
-                <Grid xs={12} item>
-                  <TextField multiline rows={4} label="Message" placeholder="Enter job details" variant="outlined" fullWidth required></TextField>
-                </Grid>
-                <Grid xs={12} item>
-                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar>
+        <Link to="/about" />
+        <Link to="/contact" />
+        <Link to="/resources" />
+      </Navbar>
+      <Stack className="header-box" spacing={2}>
+        <div>
+          <Typography sx={{ fontSize: 55 }} className="maintext">
+            Looking to post a job?
+          </Typography>
+        </div>
+        <div>
+          <Typography sx={{ fontSize: 20, mb: 3, color: '#534737' }} className="header-subtext">
+            Fill out the below form!
+          </Typography>
+        </div>
+      </Stack>
+      <Card style={{maxWidth: 550, margin: "0 auto", padding: "20px 5px"}}>
+        <CardContent>
+          <Typography variant="h5" >Contact Us</Typography>
+          <Typography variant="body2" gutterBottom component="p" color="textSecondary">Fill out the form and we will get back to you shortly.</Typography>
+          <form>
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+              <TextField label="Company Name" placeholder="Enter company name" variant="outlined" fullWidth required></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField label="Job Title" placeholder="Enter job title" variant="outlined" fullWidth required></TextField>
+            </Grid>
+            <Grid xs={12} item>
+              <TextField type="email" label="Email" placeholder="Enter email" variant="outlined" fullWidth required></TextField>
+            </Grid>
+            <Grid xs={12} item>
+              <TextField type="number" label="Phone" placeholder="Enter phone number" variant="outlined" fullWidth required></TextField>
+            </Grid>
+            <Grid xs={12} item>
+              <TextField multiline rows={4} label="Message" placeholder="Enter job details" variant="outlined" fullWidth required></TextField>
+            </Grid>
+            <Grid xs={12} item>
+              <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+            </Grid>
+          </Grid>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
